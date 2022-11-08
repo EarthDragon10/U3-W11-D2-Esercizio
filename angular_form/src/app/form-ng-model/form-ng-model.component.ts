3fbc741d0d516741f27bbb6b63a310d02cefd506
EarthDragon10 <lagudadev@gmail.com>
2022-11-08 15:16:11 +0100

    add logic ts with ngModel

--- angular_form/src/app/form-ng-model/form-ng-model.component.ts
+++ angular_form/src/app/form-ng-model/form-ng-model.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { SuperHero } from '../classes/super-hero';
 
 @Component({
   selector: 'app-form-ng-model',
   templateUrl: './form-ng-model.component.html',
-  styleUrls: ['./form-ng-model.component.scss']
+  styleUrls: ['./form-ng-model.component.scss'],
 })
 export class FormNgModelComponent implements OnInit {
+  superHero: SuperHero = new SuperHero();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  addSuperHero(): void {
+    console.log(this.superHero);
   }
-
 }
